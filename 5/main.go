f4aeaa7f68aa0ad1b283e1c9ca7a9c55b2587021
AsoloCode <artem-artem14@yandex.ru>
2023-10-27 15:22:04 +0300

    coment 5 task

--- 5/main.go
+++ 5/main.go
@@ -6,19 +6,19 @@ import (
 )
 
 func main() {
-	cnSender := make(chan string)
-	duration := 3 * time.Second
+	cnSender := make(chan string) // канал для отправки сообщений
+	duration := 5 * time.Second   // таймер для завершения работы
 	go func() {
-		timer := time.NewTimer(duration)
-		defer close(cnSender)
+		timer := time.NewTimer(duration) // устанавливаем таймер
+		defer close(cnSender)            // закрываем канал после того как завершится горутина
 		for {
 			select {
-			case <-timer.C:
+			case <-timer.C: //если таймер срабатывает то выполняется этот кейс
 				fmt.Println("Программа завершена.")
-				return
-			default:
+				return // завершаем
+			default: //если таймер еще не сработал то будет выполняться этот кейс
 				cnSender <- "Прошло пол секунды"
-				time.Sleep(time.Millisecond * 500)
+				time.Sleep(time.Millisecond * 500) //программа засыпает на 500 милисек. перед отправкой след. сообщения
 			}
 		}
 	}()
